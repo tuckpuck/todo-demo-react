f2f934cc6df4abbe60e105f0ff9aca514bf1136f
tuckpuck <tucker.triggs@yahoo.com>
2019-07-09 17:11:43 -0600

    added styling for todos

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -1,13 +1,28 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
 
 export class TodoItem extends Component {
+    getStyle = () => {
+        return {
+            background: '#f4f4f4',
+            padding: '10px',
+            borderBottom: '1px #ccc dotted',
+            textDecoration: this.props.todo.completed ? 'line-through' : 'none' 
+        }
+    }
+
     render() {
         return (
-            <div>
+            <div style={this.getStyle()}>
                 <p>{this.props.todo.title}</p>
             </div>
         )
     }
+}
+
+TodoItem.propTypes = {
+    todo: PropTypes.object.isRequired
 }
 
 export default TodoItem
